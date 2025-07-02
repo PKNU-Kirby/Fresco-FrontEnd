e7fe1813e807398c36547ae4b1fd9c29368eaa63
hnyoojin <0dbwls21@gmail.com>
2025-07-02 17:02:05 +0900

    feat: 카카오 로그인 관련 설정 및 앱 진입 구조 구현

---
+++ fresco_frontend/src/components/SocialLogin/styles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const loginButtonStyle = StyleSheet.create({
+  loginButton: {
+    width: '100%',
+  },
+  image: {
+    width: '100%',
+    height: 53,
+    resizeMode: 'contain',
+  },
+});
+
+export default loginButtonStyle;
