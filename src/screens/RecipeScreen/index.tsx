a07ed712e1c3d5ee618f8dd2d32f669e8116814f
hnyoojin <0dbwls21@gmail.com>
2025-08-03 13:48:24 +0900

    feature/#6 - first commit

--- src/screens/RecipeScreen/index.tsx
+++ src/screens/RecipeScreen/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 const RecipeScreen = () => {
   return (
     <SafeAreaView>
       <Text>RecipeScreen</Text>
+      <Text>Branch feature/#6 : Recipe Screen</Text>
     </SafeAreaView>
   );
 };
